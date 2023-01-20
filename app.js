6d2d295a8b3bf64111539c894f424f32ec3466fb
krissc84 <kriss2001@poczta.onet.pl>
2023-01-20 22:47:19 +0100

    Main game flow working

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const GameBoard = (function () {
     for (let i = 0; i < 3; i += 1) {
       newBoard[i] = new Array(3);
       for (let k = 0; k < 3; k += 1) {
-        newBoard[i][k] = j;
+        newBoard[i][k] = '';
         j += 1;
       }
     }
@@ -46,40 +46,45 @@ const GameBoard = (function () {
     console.log('Board state: ', board.toString());
     return board;
   };
-  const checkBoard = () => {
+  function checkBoard() {
     // check horizontal direction
     for (let i = 0; i < 3; i += 1) {
       if (
         board[i][0] === board[i][1] &&
-        board[i][0] === board[i][2]
+        board[i][0] === board[i][2] &&
+        board[i][0] !== ''
       ) {
-        console.log('We got a winner');
+        console.log('We got a winner',board[i][0]);
+        return board[i][0];
       }
     }
     // check vertical direction
     for (let i = 0; i < 3; i += 1) {
       if (
         board[0][i] === board[1][i] &&
-        board[0][i] === board[2][i]
+        board[0][i] === board[2][i] &&
+        board[0][i] !== ''
       ) {
-        console.log('We got a winner');
-        return;
+        console.log('We got a winner',board[0][i]);
+        return board[0][i];
       }
     }
     // check cross-directions
     if (
       board[0][0] === board[1][1] &&
-      board[0][0] === board[2][2]
+      board[0][0] === board[2][2] &&
+      board[0][0] !== ''
     ) {
-      console.log('We got a winner');
-      return;
+      console.log('We got a winner',board[0][0]);
+      return board[0][0] ;
     }
     if (
       board[0][2] === board[1][1] &&
-      board[0][2] === board[2][0]
+      board[0][2] === board[2][0] &&
+      board[0][2] !== ''
     ) {
-      console.log('We got a winner');
-      return;
+      console.log('We got a winner',board[0][2]);
+      return board[0][2];
     }
 
     // eslint-disable-next-line no-restricted-syntax
@@ -88,16 +93,19 @@ const GameBoard = (function () {
         if (board[m][n] === 'O' || board[m][n] === 'X') {
           if (n === 2 && m === 2) {
             console.log('It is a tie!!!');
-            return;
+            return 'tie';
             // break mainLoop;
           }
+          // eslint-disable-next-line no-continue
           continue;
         } else {
           break mainLoop;
         }
       }
     }
-  };
+    console.log('still in game')
+    return 0;
+  }
   const renderBoard = () => {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -121,19 +129,29 @@ const GameBoard = (function () {
 })();
 
 const GameFlow = (function () {
+
   // setup for eventlistener removal
   const controller = new AbortController();
   const controller2 = new AbortController();
   const controller3 = new AbortController();
+  const controller4 = new AbortController();
 
   // html element grabbing
   const letStart = document.querySelector('.startButton');
   const players = document.querySelector('.players');
   const mode = document.querySelector('.mode');
   const playerSings = document.querySelectorAll('.player button');
+  const boardTiles = document.querySelectorAll('.table .tile');
+  const resultInHtml =  document.querySelector('.result>h2');
 
   let PlayerMode;
   const playerSignChoice = ['', '', '', ''];
+  // eslint-disable-next-line prefer-const
+  let initialStateFinished = false;
+  const Player1 = {};
+  const Player2 = {};
+  // eslint-disable-next-line prefer-const
+  let firstPlayerMove = true;
 
   const start = () => {
     // choose the mode of the game
@@ -174,7 +192,6 @@ const GameFlow = (function () {
             element.addEventListener(
               'click',
               (e2) => {
-
                 // mark the players choice on the page and automatically mark other player sigh
                 // reset the previous player choice
                 Array.from(playerSings).forEach((element2) =>
@@ -182,45 +199,47 @@ const GameFlow = (function () {
                 );
 
                 if (e2.target.id === 'sign1') {
-                  changeButton (e2.target);
+                  changeButton(e2.target);
                   const player2 = document.querySelector('#sign4');
                   changeButton(player2);
-                  wrapPlayer (e2,player2)
-
+                  wrapPlayer(e2, player2);
                 } else if (e2.target.id === 'sign2') {
-                  changeButton (e2.target);
+                  changeButton(e2.target);
                   const player2 = document.querySelector('#sign3');
                   changeButton(player2);
-                  wrapPlayer (e2,player2)
-
+                  wrapPlayer(e2, player2);
                 } else if (e2.target.id === 'sign3') {
-                  changeButton (e2.target);
+                  changeButton(e2.target);
                   const player2 = document.querySelector('#sign2');
                   changeButton(player2);
-                  wrapPlayer (e2,player2);
-
+                  wrapPlayer(e2, player2);
                 } else if (e2.target.id === 'sign4') {
-                  changeButton (e2.target);
+                  changeButton(e2.target);
                   const player2 = document.querySelector('#sign1');
                   changeButton(player2);
-                  wrapPlayer (e2,player2);
+                  wrapPlayer(e2, player2);
                 }
+
                 console.log(playerSignChoice);
 
                 // function to reduce redundancy for injection above
-                function wrapPlayer (event,item) {
-                  playerSignChoice[0]=event.target.parentElement.firstElementChild.value;
-                  playerSignChoice[1]=event.target.innerText;
-                  playerSignChoice[2]=item.parentElement.firstElementChild.value;
-                  playerSignChoice[3]=item.innerText;
-                };
+                function wrapPlayer(event, item) {
+                  playerSignChoice[0] =
+                    event.target.parentElement.firstElementChild.value;
+                  playerSignChoice[1] = event.target.innerText;
+                  playerSignChoice[2] =
+                    item.parentElement.firstElementChild.value;
+                  playerSignChoice[3] = item.innerText;
+                }
 
                 // function to reduce redundancy for injection above
-                function changeButton (event) {
+                function changeButton(event) {
                   event.setAttribute(
                     'style',
-                    ' background-color:  hsl(180, 100%, 27%);');
+                    ' background-color:  hsl(180, 100%, 27%);'
+                  );
                 }
+                beforeFirstMove();
                 // choices can be change until the game is begin. Then the event l. will be aborted.
               },
               { signal: controller3.signal }
@@ -232,28 +251,107 @@ const GameFlow = (function () {
     }
   }
 
-  const checkMode = () => GameFlow.PlayerMode;
+  function checkMode() {
+    return GameFlow.PlayerMode;
+  }
+
+  // function that disable possibilities to change names,signs and game is started.
+  const beforeFirstMove = () => {
+    if (initialStateFinished === false) {
+      const gameBegin = () => {
+        if (initialStateFinished === false) {
+          // this state can be reached only once
+          initialStateFinished = true;
+          // now changing sing is not possible
+          controller3.abort();
+          // adding players to the game
+          GameFlow.Player1 = Player(playerSignChoice[0], playerSignChoice[1]);
+          GameFlow.Player2 = Player(playerSignChoice[2], playerSignChoice[3]);
+          // eslint-disable-next-line no-restricted-globals
+          console.log(event.target);
+          console.log(boardTiles);
+          // play first round immediately
+          // eslint-disable-next-line no-restricted-globals
+          playRound(event);
+        } else {
+          console.log('play round');
+          // eslint-disable-next-line no-restricted-globals
+          console.log(event.target);
+          // eslint-disable-next-line no-restricted-globals
+          playRound(event);
+        }
+      };
+      // adding ev.listeners to the tiles
+      Array.from(boardTiles).forEach((element3) => {
+        element3.addEventListener('click', gameBegin, {signal: controller4.signal});
+      });
+    }
+  };
+
+  function playRound(e) {
+    console.log(e.target);
+    console.log(firstPlayerMove);
+    if (firstPlayerMove === true) {
+      console.log('first player');
+      firstPlayerMove = false;
+      if (e.target.textContent === '') {
+        // e.target.textContent = GameFlow.Player1.sign;
+        pushToBoard(e, GameFlow.Player1);
+        GameBoard.renderBoard();
+        // eslint-disable-next-line prefer-const
+        let result = GameBoard.checkBoard();
+        if (result === 'X' || result === 'O') {
+          controller4.abort();
+          resultInHtml.textContent=`We got a  winner! Congratulations to ${GameFlow.Player1.name}`
+          GameBoard.gameStart();
+          GameFlow.start();
+          GameBoard.renderBoard();
+        } else if (result === 'tie') {
+          controller4.abort();
+          resultInHtml.textContent='It is a tie! Try one more time. '
+          GameBoard.gameStart();
+          GameFlow.start();
+          GameBoard.renderBoard();
+        }
+      }
+    } else {
+      console.log('second player');
+      firstPlayerMove = true;
+      if (e.target.textContent === '') {
+        // e.target.textContent = GameFlow.Player2.sign;
+        pushToBoard(e, GameFlow.Player2);
+        GameBoard.renderBoard();
+        // eslint-disable-next-line prefer-const
+        let result = GameBoard.checkBoard();
+        if (result === 'X' || result === 'O') {
+          controller4.abort();
+          resultInHtml.textContent=`We got a  winner! Congratulations to ${GameFlow.Player1.name}`
+          GameBoard.gameStart();
+          GameFlow.start();
+          GameBoard.renderBoard();
+        } else if (result === 'tie') {
+          controller4.abort();
+          resultInHtml.textContent='It is a tie! Try one more time. ';
+          
+          GameBoard.gameStart();
+          GameFlow.start();
+          GameBoard.renderBoard();
+        }
+      }
+    }
+  }
+  // refreshing board object
+  const pushToBoard = (e, player) => {
+    console.log(player);
+    console.log(e.target.id[3], e.target.id[5]);
+    GameBoard.makeMove(player, [e.target.id[3], e.target.id[5]]);
+  };
 
   console.log(PlayerMode);
-  return { start, checkMode, playerSignChoice };
+
+  return { start, checkMode, playerSignChoice, Player1, Player2 };
 })();
 
 GameBoard.gameStart();
 GameFlow.start();
 
-// const player1 = Player('Kriss', 'O');
-// const player2 = Player('Cross', 'X');
-
-// GameBoard.gameStart();
-
-// GameBoard.makeMove(player1, [1, 1]);
-// GameBoard.makeMove(player1, [2, 1]);
-// GameBoard.makeMove(player1, [0, 2]);
-// GameBoard.makeMove(player2, [0, 1]);
-// GameBoard.makeMove(player2, [1, 0]);
-// GameBoard.makeMove(player2, [2, 0]);
-// GameBoard.makeMove(player2, [1, 2]);
-// GameBoard.makeMove(player2, [2, 2]);
-// GameBoard.boardState();
-// GameBoard.checkBoard();
-// GameBoard.renderBoard();
